040f5d277cdf93e6b467650bd45e5f1264d8ba12
vladkudino89 <vlad.kudinov.89@gmail.com>
2019-10-01 10:48:17 +0300

    Add methods and described them.

---
+++ app/Repositories/Task/TaskRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Repositories\Task;
+
+use App\Entities\Task;
+use Illuminate\Database\Eloquent\Collection;
+
+class TaskRepository implements TaskRepositoryInterface
+{
+    public function findAll(): Collection
+    {
+        return Task::all();
+    }
+
+    public function getById(int $id): ?Task
+    {
+        return Task::find($id);
+    }
+
+    public function save(Task $task): Task
+    {
+        $task->save();
+
+        return $task;
+    }
+
+    public function deleteById(int $id): void
+    {
+        return Task::destroy($id);
+    }
+
+}
