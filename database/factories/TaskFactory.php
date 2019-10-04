3bad0719c9a1c831663a2045e4b11900113c85c5
vladkudino89 <vlad.kudinov.89@gmail.com>
2019-10-04 18:54:53 +0300

    Fix TaskFactory sentence length.

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Task::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
-        'description' => $faker->sentence($nbWords = 4, $variableNbWords = true),
+        'description' => $faker->sentence($nbWords = 3, $variableNbWords = true),
         'status' => $faker->boolean
     ];
 });
