9a55fd82bc6b760c3f855a74241fefb5e12645d5
vladkudino89 <vlad.kudinov.89@gmail.com>
2019-11-19 21:46:25 +0300

    Add elasticsearch to project. Add container. Add console command to init elastic data.

---
+++ app/Console/Commands/Search/InitCommand.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Console\Commands\Search;
+
+use App\Entities\Task;
+use App\Repositories\Task\TaskRepository;
+use Elasticsearch\Client;
+use Elasticsearch\Common\Exceptions\Missing404Exception;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class InitCommand extends Command
+{
+    protected $signature = 'search:init';
+
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        parent::__construct();
+        $this->client = $client;
+    }
+
+    public function handle(): bool
+    {
+        $this->initTasks();
+
+        return true;
+    }
+
+    private function initTasks()
+    {
+        try {
+            $this->client->indices()->delete([
+                'index' => 'app'
+            ]);
+        } catch (Missing404Exception $e) {
+        }
+
+        $this->client->indices()->create([
+           'index' => 'app',
+            'body' => [
+                'mappings' =>[
+                    'tasks' => [
+                        'properties' => [
+                            'id' => [
+                                'type' => 'integer'
+                            ],
+                            'name' => [
+                                'type' => 'text'
+                            ],
+                            'description' => [
+                                'type' => 'text'
+                            ],
+                            'status' => [
+                                'type' => 'integer'
+                            ],
+                        ]
+                    ]
+                ],
+
+                'settings' => [
+                    'analysis' => [
+                        'char_filter' => [
+                            'replace' => [
+                                'type' => 'mapping',
+                                'mappings' => [
+                                    '&=> and '
+                                ],
+                            ],
+                        ],
+                        'filter' => [
+                            'word_delimiter' => [
+                                'type' => 'word_delimiter',
+                                'split_on_numerics' => false,
+                                'split_on_case_change' => true,
+                                'generate_word_parts' => true,
+                                'generate_number_parts' => true,
+                                'catenate_all' => true,
+                                'preserve_original' => true,
+                                'catenate_numbers' => true,
+                            ],
+                            'trigrams' => [
+                                'type' => 'ngram',
+                                'min_gram' => 4,
+                                'max_gram' => 6,
+                            ],
+                        ],
+                        'analyzer' => [
+                            'default' => [
+                                'type' => 'custom',
+                                'char_filter' => [
+                                    'html_strip',
+                                    'replace',
+                                ],
+                                'tokenizer' => 'whitespace',
+                                'filter' => [
+                                    'lowercase',
+                                    'word_delimiter',
+                                    'trigrams',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ]
+        ]);
+
+        foreach (DB::table('task_lists')
+                     ->orderBy('id')->cursor() as $task) {
+            $this->client->index([
+                'index' => 'app',
+               'type' => 'tasks',
+               'id' => $task->id,
+                'body' => [
+                    'id' => $task->id,
+                    'name' => $task->name,
+                    'description' => $task->description,
+                    'status' => $task->status,
+                ]
+            ]);
+        }
+
+
+    }
+}
